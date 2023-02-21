649aafb3a61b60ee03433ce3eee91471a2008871
Nicholaskogei <kogein579@gmail.com>
2023-02-21 04:14:12 -0800

    I sometimes suffer from insomnia.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - entry point
+ * Description: To write a to z embedd it in a function
+ * Return: 0 if successful
+ */
+void print_alphabet(void)
+{
+	char let = 'a';
+
+	while (let <= 'z')
+	{
+		_putchar(let);
+		let++;
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,18 +1,13 @@
-#include "main.h"
-/**
- * main - main  entry poin
- *
- * Return: 0 if succes
- */
-int main(void)
-{
-	char printput[8] = "_putchar";
-	int step;
+#include <unistd.h>
 
-	for (step = 0; step < 8; step++)
-	{
-		_putchar(printput[step]);
-	}
-	_putchar('\n');
-	return (0);
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
